c66b200c68068cea5d082a7594a499d114971e24
Petar <parandjic@gmail.com>
2019-04-03 10:26:44 +0200

    secutity update to database class

--- data/classes/database/Database.php
+++ data/classes/database/Database.php
@@ -6,21 +6,24 @@
 		private $dbname;
 		private $charset;
 
-		protected function connect(){
+		protected function set_db_parametars(){
 			$this->hostname = 'localhost';
 			$this->username = 'root';
 			$this->password = '';
-			$this->dbname = 'cargo_navis';
+			$this->dbname = 'test2';
 			$this->charset = 'utf8mb4';
+		}
+		protected function connect(){
 
+
+			$dsn = 'mysql:host=' . $this->servername . ';dbname=' . $this->dbname . ';charset=' . $this->charset;
+			
 			try{
-				$dsn = 'mysql:host=' . $this->servername . ';dbname=' . $this->dbname . ';charset=' . $this->charset;
 				$pdo = new PDO($dsn, $this->username, $this->password);
 				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				return $pdo;
-
 			}catch (PDOException $e){
 				echo 'connection error' . $e->getMessage;
-			}
+			}			
 		}
 	}
