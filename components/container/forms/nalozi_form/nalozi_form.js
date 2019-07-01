a6a5602d00fc0828e960aa0fc87f41808aeaa000
Petar <parandjic@gmail.com>
2019-07-01 20:50:52 +0200

    nalozi form now can be used to update old data

--- components/container/forms/nalozi_form/nalozi_form.js
+++ components/container/forms/nalozi_form/nalozi_form.js
@@ -1,12 +1,26 @@
 //variable used insede save_id_partnera
 var nfKomitent = "N/A";
-
-function show_nalozi_form(){
+var nal_up_or_ins = 0;
+function show_nalozi_form(wtd){
     var form = document.getElementById("nalozi_form");
     if(form.style.display == "block"){
         form.style.display = "none";
     }else{
         form.style.display = "block";
+    }
+    if (wtd == 1) {
+        nal_up_or_ins = 1;
+        document.getElementById("nf-komitenti").value = s_data.nalozi[active_element].ime_partnera
+        document.getElementById("nf-paritet").value =  s_data.nalozi[active_element].paritent
+        document.getElementById("nf-bl").value =  s_data.nalozi[active_element].bl
+        document.getElementById("nf-bl2").value =  s_data.nalozi[active_element].bl2
+        document.getElementById("nf-luka_polaska").value =  s_data.nalozi[active_element].luka_polazka
+        document.getElementById("nf-luka_dolaska").value =  s_data.nalozi[active_element].luka_dolaska
+        document.getElementById("nf-datum_polaska").value =  s_data.nalozi[active_element].datum_polaska
+        document.getElementById("nf-datum_dolaska").value =  s_data.nalozi[active_element].datum_dolaska
+        document.getElementById("nf-datum_ponude").value =  s_data.nalozi[active_element].datum_ponude
+    }else if (wtd == 0) {
+        nal_up_or_ins = 0;
     }
 }
 //reset nalozi form
@@ -53,22 +67,46 @@ function save_id_partnera(){
     }
 }
 function save_nalog_to_db(){
-    var data = {
-        insert:{
-            table_name: "poslovi",
-            data: [
-                {
+    if (nal_up_or_ins == 0) {
+        var data = {
+            insert:{
+                table_name: "poslovi",
+                data: [
+                    {
+                        id_partneri: nfKomitent,
+                        paritent: document.getElementById("nf-paritet").value,
+                        bl: document.getElementById("nf-bl").value,
+                        bl2: document.getElementById("nf-bl2").value,
+                        luka_polazka: document.getElementById("nf-luka_polaska").value,
+                        luka_dolaska: document.getElementById("nf-luka_dolaska").value,
+                        datum_polaska: document.getElementById("nf-datum_polaska").value,
+                        datum_dolaska: document.getElementById("nf-datum_dolaska").value,
+                        datum_ponude: document.getElementById("nf-datum_ponude").value
+                    }
+                ]
+            }
+   
+        }
+    }else if (nal_up_or_ins == 1) {
+        var data = {
+            update: {
+                table_name: "poslovi",
+                collums:{
                     id_partneri: nfKomitent,
-                    paritent: document.getElementById("nf-paritet").value,
-                    bl: document.getElementById("nf-bl").value,
-                    bl2: document.getElementById("nf-bl2").value,
-                    luka_polazka: document.getElementById("nf-luka_polaska").value,
-                    luka_dolaska: document.getElementById("nf-luka_dolaska").value,
-                    datum_polaska: document.getElementById("nf-datum_polaska").value,
-                    datum_dolaska: document.getElementById("nf-datum_dolaska").value,
-                    datum_ponude: document.getElementById("nf-datum_ponude").value,
+                        paritent: document.getElementById("nf-paritet").value,
+                        bl: document.getElementById("nf-bl").value,
+                        bl2: document.getElementById("nf-bl2").value,
+                        luka_polazka: document.getElementById("nf-luka_polaska").value,
+                        luka_dolaska: document.getElementById("nf-luka_dolaska").value,
+                        datum_polaska: document.getElementById("nf-datum_polaska").value,
+                        datum_dolaska: document.getElementById("nf-datum_dolaska").value,
+                        datum_ponude: document.getElementById("nf-datum_ponude").value
+                },
+                index: {
+                    index_name: "id",
+                    index_value: s_data.nalozi[active_element].id
                 }
-            ]
+            }
         }
     }
     //console.log(data)
@@ -85,13 +123,11 @@ function save_nalog_to_db_aj(to_server){
             //console.log(this.responseText)
             $server_response = JSON.parse(this.responseText);
             //console.log($server_response);
-            if($server_response.insert.messange == 101){
-                //call function from get_data.js from global variables - get latest saved data
-                alert("Sačuvano");
-                set_select_database_par();
-                show_nalozi_form();
-                reset_nalozi_form();
-            }
+            //call function from get_data.js from global variables - get latest saved data
+            alert("Sačuvano");
+            set_select_database_par();
+            show_nalozi_form();
+            reset_nalozi_form();
         }
     };
     //send data
